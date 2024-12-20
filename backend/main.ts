bcb2eba3f2f84b79b1e78166ad3e0f73a9d7b1a1
AahilRafiq <aahil.rafiq136@gmail.com>
2024-12-20 09:15:30 +0530

    add run get endpoint in backend for longpoll

--- backend/main.ts
+++ backend/main.ts
@@ -1,6 +1,7 @@
 // @deno-types="npm:@types/express@4"
 import express, { Request, Response } from "express";
 import { RunRequestBody } from "./types/RunRequestBody.ts"
+import { RunResponseBody } from "./types/RunResponseBody.ts";
 import {Queue} from 'bullmq'
 import IORedis from 'ioredis'
 
@@ -17,6 +18,20 @@ app.post("/run", async (req: Request, res: Response) => {
     res.sendStatus(200);
 });
 
+app.get("/run", async (req: Request, res: Response) => {
+    const userId = req.query.userId as string
+    const questionId = req.query.questionId as string
+    const key = 'run' + userId + questionId
+    const outputString = await redis.get(key)
+    if (outputString === null) {
+        res.sendStatus(204)
+        return
+    }
+
+    const output: RunResponseBody = JSON.parse(outputString)
+    await redis.del(key)
+    res.json(output)
+})
 
 app.listen(port, () => {
     console.log(`Listening on ${port} ...`);
--- ce-api/helpers/handleTask.ts
+++ ce-api/helpers/handleTask.ts
@@ -4,7 +4,7 @@ import type { RedisType } from "../main.ts";
 export async function handleTask(redis: RedisType , task:QueueItem) {
     try {
 
-        const keyToOutput: string | undefined = task.userId;
+        const keyToOutput: string | undefined = 'run' + task.userId.toString() + task.questionId.toString();
         const output = await fetch("https://emkc.org/api/v2/piston/execute", {
             method: "POST",
             headers: {
@@ -16,7 +16,9 @@ export async function handleTask(redis: RedisType , task:QueueItem) {
             throw new Error("Error in fetching output")
         }
         const json_output = await output.json();
-        await redis.set(keyToOutput, JSON.stringify(json_output));
+
+        // Expires in 15 seconds
+        await redis.set(keyToOutput, JSON.stringify(json_output), 'EX', 20);
         console.log(json_output)
 
     } catch (err) {
