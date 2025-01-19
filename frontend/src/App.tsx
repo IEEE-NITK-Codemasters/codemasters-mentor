dcf0d7ad4480bcb6f18be9b76613fa1855e5fe67
AahilRafiq <aahil.rafiq136@gmail.com>
2025-01-19 12:19:17 +0530

    add signup and sign in pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import CodeEditor from "@/pages/CodeEditor";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
 import Navbar from "@/components/Navbar";
+import Signup from "./pages/auth/Signup";
+import Signin from "./pages/auth/Signin";
+import { Toaster } from "@/components/ui/toaster"
 
 const App: React.FC = () => {
   return (
     <>
       <Navbar />
+      <Toaster />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/question/:id" element={<CodeEditor />} />
-
+        <Route path="/auth/signup" element={<Signup />} />
+        <Route path="/auth/signin" element={<Signin />} />
       </Routes>    
     </>
   );
