cb70cb248bc34cfb49c801087f396278124b604a
AahilRafiq <aahil.rafiq136@gmail.com>
2025-02-12 22:58:25 +0530

    minor change

--- frontend/src/helpers/question/getQuestion.ts
+++ frontend/src/helpers/question/getQuestion.ts
@@ -1,7 +1,7 @@
 import type { Question } from "@/types/question";
 
 export async function getQuestion(id: number): Promise<Question> {
-    const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/questions/${id}`);
+    const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/question/${id}`);
     if (!response.ok) {
         console.error(response)
         throw new Error(`Failed to fetch question with id ${id}: ${response.statusText}`);
